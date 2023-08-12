9cf5412f0aaf2f82819422558c3bc2526fd9f920
3absy1 <yossfeabdaslm123@gmail.com>
2023-08-12 08:28:04 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: When I was having that alphabet soup
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'q' || ch == 'e')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
