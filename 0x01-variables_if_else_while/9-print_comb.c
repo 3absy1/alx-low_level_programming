a75b81c8faa12abbcd0c14df6bbae3441ce31dc4
3absy1 <yossfeabdaslm123@gmail.com>
2023-08-12 09:13:00 -0700

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Patience, persistence and perspiration
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + 48);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
