0f13e41ffc886970f6b5f974de521c6abb933eda
3absy1 <yossfeabdaslm123@gmail.com>
2023-08-16 17:40:37 -0700

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,9 +26,9 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prob / 10) + 48);
+				_putchar((prod / 10) + 48);
 			_putchar((prod % 10) + 48);
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
