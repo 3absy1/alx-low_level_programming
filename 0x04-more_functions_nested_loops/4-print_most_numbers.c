4fab36dca75ee1fc3c4328adb4a5badb0a249f96
3absy1 <yossfeabdaslm123@gmail.com>
2023-08-18 08:19:14 -0700

    update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9 but not 2 and 4
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}
