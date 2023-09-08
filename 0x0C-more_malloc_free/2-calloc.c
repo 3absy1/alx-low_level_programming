615624ef4fcbc2a1882604c71382bf5326671c24
3absy1 <yossfeabdaslm123@gmail.com>
2023-09-08 06:53:35 -0700

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *ptr = s;
+	unsigned int i;
 
-	while (n--)
-		*s++ = b;
-	return (ptr);
+	for ( i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
 }
 
 /**
@@ -27,15 +28,15 @@ char *_memset(char *s, char b, unsigned int n)
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *m;
+	char *m;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	m = malloc(sizeof(int) * nmemb);
+	m = malloc(size * nmemb);
 
-	if (m == 0)
+	if (m == NULL)
 		return (NULL);
-	_memset(m, 0, sizeof(int) * nmemb);
+	_memset(m, 0, size * nmemb);
 
 	return (m);
 }
