eaa97e87061aeadb5936c58180dadb67072a73bd
3absy1 <yossfeabdaslm123@gmail.com>
2023-09-12 11:08:07 -0700

    update

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free dog
+ * @d: parameter
+ *
+ * Return: void
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
